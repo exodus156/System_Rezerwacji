345d39e0bf21840c23071f7c264ea36faafa5ada
Michal Dzialek <exodusofdead@gmail.com>
2020-09-17 18:54:08 +0200

    Added graphQL support to server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,21 @@
 const express = require('express');
-
+const cors = require('cors');
+const {graphqlHTTP} = require('express-graphql');
+const mongoose = require('mongoose');
+const schema = require('./schema/schema');
 
 const app = express();
+
+//app.use(cors); //Enable cross-section connection
+
+/* Mongoose connection to database start */
+
+/* Mongoose connection to database end */
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true //for testing connection, will remove later
+})); //Binding GraphQL with Express
 
 app.listen(4000, () =>{
     console.log("listening to port 4000");
